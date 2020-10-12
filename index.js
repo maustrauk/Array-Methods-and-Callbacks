03fa6c4c31496e4df9ba43c4277e356c563c6a92
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-12 17:49:01 -0400

    mvp 2.4

--- index.js
+++ index.js
@@ -55,13 +55,22 @@ console.log(years);
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-
-    /* code here */
-
+function getWinners(callback) {
+    const userArray = callback(fifaData);
+    const resultArray = [];
+    for (let i = 0; i < userArray.length; i++) {
+        const final = userArray[i];
+        if (final["Home Team Goals"] > final["Away Team Goals"]) {
+            resultArray.push(final["Home Team Name"]);
+        } else {
+            resultArray.push(final["Away Team Name"]);
+        }
+    }
+    return resultArray;
 };
 
-getWinners();
+const winners = getWinners(getFinals);
+console.log(winners);
 
 /* Task 5: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 
