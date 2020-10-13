f598588dc1cbfa131a244d1975352c13c5b75697
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-13 16:44:01 -0400

    mvp 3.4

--- stretch.js
+++ stretch.js
@@ -50,3 +50,32 @@ function goalCounter (data, teamInitial) {
 }
 
 console.log("Total number of Soviet Union goals = ", goalCounter(fifaData, "URS"));
+
+//Task 4
+function creatingH1 (h1Text) {
+
+    // create a couple of elements in an otherwise empty HTML page
+    const heading = document.createElement("h1");
+    const heading_text = document.createTextNode(h1Text);
+    heading.appendChild(heading_text);
+    document.body.appendChild(heading);
+ }
+
+ function teamNames (data) {
+     const homeTeamNames = data.map(match => match = match["Home Team Name"]);
+     const awayTeamNames = data.map(match => match = match["Away Team Name"]);
+     const rawTeamNames = homeTeamNames.concat(awayTeamNames);
+     const teamNamesArray = removeDuplicateInArray(rawTeamNames);
+     return teamNamesArray;
+ }
+
+
+function h1Creation (data) {
+    const userArray = teamNames(data);
+    for (let i = 0; i < userArray.length; i++) {
+        creatingH1(userArray[i]);
+    }
+}
+
+h1Creation (fifaData);
+
